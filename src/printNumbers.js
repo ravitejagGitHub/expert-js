67aac8592cf697cde80eb9d2bb63a602d82a1222
ravitejagGitHub <raviteja.giduturi@gmail.com>
2021-08-02 16:14:49 +0530

    print n umbers

--- src/printNumbers.js
+++ src/printNumbers.js
@@ -4,4 +4,14 @@ function printN(n) {
     }
 };
 
-printN(5);
+function printN2(n) {
+    for (var i = 1; i <= n; i++) {
+        (function(i) {
+            setTimeout(() => console.log(i), 1000 * i);
+        })(i)
+    }
+};
+
+
+
+printN2(5);
